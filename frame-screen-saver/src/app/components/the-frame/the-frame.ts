74be71a4e7a4e0af582d0d797562f5e3cb30f28d
Montefiori Adéna Auróra <adena.montefiori@gmail.com>
2025-09-06 00:56:36 +0200

    feat(Black-Frame): :art: initial implementation
    Initial implementation of the black frame, displaying a series of images through splide (todo: not clickable buttons, hide buttons and automate movements)

--- frame-screen-saver/src/app/components/the-frame/the-frame.ts
+++ frame-screen-saver/src/app/components/the-frame/the-frame.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgxSplideModule } from 'ngx-splide';
 
 @Component({
   selector: 'app-the-frame',
-  imports: [],
   templateUrl: './the-frame.html',
-  styleUrl: './the-frame.scss'
+  styleUrl: './the-frame.scss',
+  imports: [ NgxSplideModule ],
+  standalone: true
 })
+
 export class TheFrame {
 
 }
