4d4bda5ae8a66fcbf40254e63cfaa4bb6897c822
Montefiori Adéna Auróra <adena.montefiori@gmail.com>
2025-09-05 23:19:31 +0200

    feat: initia\, resizeable still image frame image.

---
+++ frame-screen-saver/src/app/components/the-frame/the-frame.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TheFrame } from './the-frame';
+
+describe('TheFrame', () => {
+  let component: TheFrame;
+  let fixture: ComponentFixture<TheFrame>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TheFrame]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TheFrame);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frame-screen-saver/src/app/components/the-frame/the-frame.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-the-frame',
+  imports: [],
+  templateUrl: './the-frame.html',
+  styleUrl: './the-frame.scss'
+})
+export class TheFrame {
+
+}
