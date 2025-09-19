86870ae617ea7e4f6f31cbb82b67b17a195670db
Montefiori Adéna Auróra <adena.montefiori@gmail.com>
2025-09-19 21:08:43 +0200

    feat: animations between pages

--- frame-screen-saver/src/app/components/the-frame/the-frame.ts
+++ frame-screen-saver/src/app/components/the-frame/the-frame.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 import { NgxSplideModule } from 'ngx-splide';
+import { Component, isDevMode } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-the-frame',
@@ -8,7 +9,50 @@ import { NgxSplideModule } from 'ngx-splide';
   imports: [ NgxSplideModule ],
   standalone: true
 })
-
 export class TheFrame {
+  IMAGE_DISPLAY_TIMEOUT_MS: number = isDevMode() ? 2000 : 8000;
+  COVER_PAGE_DISPLAY_TIMEOUT_MS: number = isDevMode() ? 2000 : 8000;
+  COLUMNS_PAGE_DISPLAY_TIMEOUT_MS: number = isDevMode() ? 2000 : 8000;
 
+  onSplideInit(splide: any)
+  {
+    var tl = gsap.timeline();
+
+    tl.add('page2')
+      .to("#screensaver-info-pages", {
+        opacity: 1,
+        delay: this.IMAGE_DISPLAY_TIMEOUT_MS / 1000
+      }, 'page2')
+      .to("#screensaver-info-pages .cover-page", {
+        opacity: 1,
+        x: 0,
+        delay: this.IMAGE_DISPLAY_TIMEOUT_MS / 1000
+      }, 'page2');
+
+      tl.add('page3')
+      .to("#screensaver-info-pages .cover-page", {
+        opacity: 0,
+        x: 32,
+        delay: this.COVER_PAGE_DISPLAY_TIMEOUT_MS / 1000
+      }, 'page3')
+      .to("#screensaver-info-pages .columns-page", {
+        opacity: 1,
+        x: 0,
+        delay: this.COVER_PAGE_DISPLAY_TIMEOUT_MS / 1000
+      }, 'page3');
+
+      tl.add('transient')
+        .to('#screensaver-info-pages', {
+          opacity: 0,
+          delay: this.COLUMNS_PAGE_DISPLAY_TIMEOUT_MS / 1000
+        }, 'transient')
+        .to('#screensaver-info-pages .columns-page', {
+          opacity: 0,
+          x: 32,
+          delay: this.COLUMNS_PAGE_DISPLAY_TIMEOUT_MS / 1000
+        }, 'transient').call(() => {
+          splide.go('>');
+          tl.restart();
+        });
+  }
 }
